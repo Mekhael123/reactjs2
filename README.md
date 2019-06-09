6f0fd37351d0e4675b77c4ee64d20ceafacef00c
Mekhael <michaeladel999@gmail.com>
2019-06-09 21:31:55 +0200

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+﻿This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
@@ -45,7 +45,8 @@ To learn React, check out the [React documentation](https://reactjs.org/).
 
 ### Code Splitting
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+This 
+section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
 
 ### Analyzing the Bundle Size
 
---
+++ src/components/reducers/cartReducer.js
@@ -0,0 +1,103 @@
+import Item1 from '../../images/img1.jpg'
+import Item2 from '../../images/img2.jpg'
+import Item3 from '../../images/img4.jpg'
+import Item4 from '../../images/img5.jpg'
+import Item5 from '../../images/img6.jpg'
+import Item6 from '../../images/img3.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
+
+
+const initState = {
+    items: [
+        {id:1, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:10,img:Item1},
+        {id:2, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:80,img: Item2},
+        {id:3, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:20,img: Item3},
+        {id:4, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:60,img:Item4},
+        {id:5, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.", price:60,img: Item5},
+        {id:6, desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, ex.",price:9,img: Item6}
+    ],
+    addedItems:[],
+    total: 0
+
+}
+const cartReducer= (state = initState,action)=>{
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+
+
+
+
+    return state
+}
+
+export default cartReducer
